6353dbcc9a96dcaae204b112a6de9ed235828666
GiovanniMiglio <gmiglio89@gmail.com>
2025-09-15 12:04:33 +0200

    svolto esercizio2

--- php_01_nome_cognome/index.php
+++ php_01_nome_cognome/index.php
@@ -1,10 +1,19 @@
 <?php
-$Integer = 36;
-$Float = 181.5;
-$String = "Giovanni";
-$Boolean = true;
-var_dump($Integer, $Float, $String, $Boolean);
-const Integer = 36;
-const Float = 181.5;    
-const String = "Giovanni";
-const Boolean = true;
+// $Integer = 36;
+// $Float = 181.5;
+// $String = "Giovanni";
+// $Boolean = true;
+// var_dump($Integer, $Float, $String, $Boolean);
+// const Integer = 36;
+// const Float = 181.5;    
+// const String = "Giovanni";
+// const Boolean = true;
+$text1 = "Marco";
+$text2 = "hai";
+$text3 = "sete";
+$text4 = "?";
+$text5 = "Perché";
+$text7 = "bevuto";
+$text8 = "tutto";
+$frase = "$text1 $text2 $text3 $text4 $text5 $text2 $text7 $text8";
+echo $frase;
