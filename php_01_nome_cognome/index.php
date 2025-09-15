94465640de95a4198c9c26c819fc8691f2068431
GiovanniMiglio <gmiglio89@gmail.com>
2025-09-15 11:51:16 +0200

    Aggiungo esercizio nome-cognome

---
+++ php_01_nome_cognome/index.php
@@ -0,0 +1,10 @@
+<?php
+$Integer = 36;
+$Float = 181.5;
+$String = "Giovanni";
+$Boolean = true;
+var_dump($Integer, $Float, $String, $Boolean);
+const Integer = 36;
+const Float = 181.5;    
+const String = "Giovanni";
+const Boolean = true;
