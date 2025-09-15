7900dd1f04124c199ac47487b330bd38827dc8b4
GiovanniMiglio <gmiglio89@gmail.com>
2025-09-15 16:34:42 +0200

    svolto esercizio2

--- php_02_nome_cognome/index.php
+++ php_02_nome_cognome/index.php
@@ -1,18 +1,26 @@
 <?php
-$users = [
-    ['name' => 'Davide', 'surname' => 'Cariola', 'gender' => 'NB'],
-    ['name' => 'Giulia', 'surname' => 'Rossi', 'gender' => 'F'],
-    ['name' => 'Luca', 'surname' => 'Bianchi', 'gender' => 'M'],
-    ['name' => 'Sam', 'surname' => 'Verdi', 'gender' => 'NB']
-];
+// $users = [
+//     ['name' => 'Davide', 'surname' => 'Cariola', 'gender' => 'NB'],
+//     ['name' => 'Giulia', 'surname' => 'Rossi', 'gender' => 'F'],
+//     ['name' => 'Luca', 'surname' => 'Bianchi', 'gender' => 'M'],
+//     ['name' => 'Sam', 'surname' => 'Verdi', 'gender' => 'NB']
+// ];
 
-foreach ($users as $user) {
-    if ($user['gender'] === 'M'){
-        $saluta= "Buongiorno Sig " . $user['name'] . " " . $user['surname'] . "\n";
-    }elseif ($user['gender'] === 'F'){
-        $saluta= "Buongiorno Sig.ra " . $user['name'] . " " . $user['surname'] . "\n";
-    }else{
-        $saluta= "Buongiorno " . $user['name'] . " " . $user['surname'] . "\n";
-    }
-    echo $saluta;
+// foreach ($users as $user) {
+//     if ($user['gender'] === 'M'){
+//         $saluta= "Buongiorno Sig " . $user['name'] . " " . $user['surname'] . "\n";
+//     }elseif ($user['gender'] === 'F'){
+//         $saluta= "Buongiorno Sig.ra " . $user['name'] . " " . $user['surname'] . "\n";
+//     }else{
+//         $saluta= "Buongiorno " . $user['name'] . " " . $user['surname'] . "\n";
+//     }
+//     echo $saluta;
+// };
+$numeri = [42, 87, 15, 63, 29, 94, 8, 76, 51, 33];
+$somma = 0;
+foreach($numeri as $numero) {
+   if($numero % 2 === 0) { 
+    $somma += $numero;
+   }
 };
+echo "La somma dei numeri pari è: " . $somma;
