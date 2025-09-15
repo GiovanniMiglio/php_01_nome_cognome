321cb9300f2de476a07f8ff66c0ebb711181fa7b
GiovanniMiglio <gmiglio89@gmail.com>
2025-09-15 16:50:02 +0200

    svolto esercizio 3

--- php_02_nome_cognome/index.php
+++ php_02_nome_cognome/index.php
@@ -16,11 +16,23 @@
 //     }
 //     echo $saluta;
 // };
-$numeri = [42, 87, 15, 63, 29, 94, 8, 76, 51, 33];
-$somma = 0;
-foreach($numeri as $numero) {
-   if($numero % 2 === 0) { 
-    $somma += $numero;
-   }
-};
-echo "La somma dei numeri pari è: " . $somma;
+// $numeri = [42, 87, 15, 63, 29, 94, 8, 76, 51, 33];
+// $somma = 0;
+// foreach($numeri as $numero) {
+//    if($numero % 2 === 0) { 
+//     $somma += $numero;
+//    }
+// };
+// echo "La somma dei numeri pari è: " . $somma;
+
+for ($i = 1; $i <= 100; $i++) {
+    if (($i % 3 === 0 && $i % 5 === 0)) {
+        echo "HACKADEMY" . "\n";
+    }elseif ($i % 5 === 0) {
+        echo "JAVASCRIPT" . "\n";
+    }elseif ($i % 3 === 0) {
+        echo "PHP" . "\n";
+    }else{
+        echo $i . "\n";
+    }
+}
