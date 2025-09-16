13dcbf72d0b08b91dae9a6f5f3fb81138f4bbebf
GiovanniMiglio <gmiglio89@gmail.com>
2025-09-16 18:08:10 +0200

    esercizio completato

--- php_password/psw.php
+++ php_password/psw.php
@@ -1,14 +1,43 @@
 <?php
-$password = readline("Inserisci la password: ");
-$pswlenghth = strlen($password);
-
-function checklength($pswlenghth){
-    for($i=0; $i<$pswlenghth; $i++){
-        if($pswlenghth < 8){
-            return "Password troppo corta";
-        } else {
-            return "Password accettata";
+function contieneNumero($password) {
+    for ($i = 0; $i < strlen($password); $i++) {
+        if (is_numeric($password[$i])) {
+            return true;
         }
     }
+    return false;
 }
-echo checklength($pswlenghth);
+function contieneLetteraMaiuscola($password) {
+    for ($i = 0; $i < strlen($password); $i++) {
+        if (ctype_upper($password[$i])) {
+            return true;
+        }
+    }
+    return false;
+}
+function contieneSpeciali($password) {
+    return preg_match('/[\W]/', $password);
+}
+function richiestaPassword() {
+    do {
+        $password = readline("Inserisci la password: ");
+        $length = strlen($password);
+
+        if (empty($password)) {
+            echo "La password non può essere vuota.\n";
+        } elseif ($length < 8) {
+            echo "Password troppo corta. Deve avere almeno 8 caratteri.\n";
+        } elseif (!contieneNumero($password)) {
+            echo "La password deve contenere almeno un numero.\n";
+        } elseif (!contieneLetteraMaiuscola($password)) {
+            echo "La password deve contenere almeno una lettera maiuscola.\n";
+        } elseif (!contieneSpeciali($password)) {
+            echo "La password deve contenere almeno un carattere speciale.\n";
+        } else {
+            echo "Password accettata!\n";
+            return $password;
+        }
+
+    } while (true);
+}     
+$password = richiestaPassword();
